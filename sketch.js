0d854649b3bfd0c525ecbd47ca05aa44e9e8186a
nameenbutt <nameenbut20@gmail>
2026-02-03 15:08:55 +0000

    save

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
 function setup() {
-  createCanvas(500,500);
+  createCanvas(400,400);
   background(225);
-  circle(55,55,20);
+  rectMode(CENTER);
 }
 
 function draw() {
 }
+
+//DRAW THE PURPLE RECTANGLE//
+fill(111, 50,168);
+rect(100,100,200);
+
+//DRAW THE RED RECTANGLE//
+fill(235,64,52);
+rect(175,200,250,50);
+
+//DRAW THE PINK CIRCLE//
+ellipseMode(CENTER);
+fill(252,3,169);
+circle(150,150,150);
