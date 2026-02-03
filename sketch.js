b8ff1a29cf58f311e3cb98b8531256545d698416
nameenbutt <nameenbut20@gmail>
2026-02-03 15:11:54 +0000

    new functions

--- sketch.js
+++ sketch.js
@@ -7,14 +7,20 @@ function setup() {
 function draw() {
 }
 
+function drawpurplerectangle() {
+}
 //DRAW THE PURPLE RECTANGLE//
 fill(111, 50,168);
 rect(100,100,200);
 
+function drawredrectangle() {
+}
 //DRAW THE RED RECTANGLE//
 fill(235,64,52);
 rect(175,200,250,50);
 
+function drawpinkcircle() {
+}
 //DRAW THE PINK CIRCLE//
 ellipseMode(CENTER);
 fill(252,3,169);
